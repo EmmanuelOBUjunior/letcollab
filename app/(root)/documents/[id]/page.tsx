a343ad13074a0f32f340685574af55d73290c974
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-15 22:00:56 +0000

    Added the header into the document editor screen

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,8 +1,12 @@
 import { Editor } from "@/components/editor/Editor"
+import Header from "@/components/Header"
 
 const Document = () => {
   return (
     <div>
+      <Header>
+        <p>Text</p>
+      </Header>
       <Editor/>
     </div>
   )
