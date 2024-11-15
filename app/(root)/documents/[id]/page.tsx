cf8a77c3ed76c99492c9d7eee85fab25c931cc02
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-15 23:12:34 +0000

    Working on the  collaborative room

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,6 +1,3 @@
-import { Editor } from "@/components/editor/Editor";
-import Header from "@/components/Header";
-import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 
 const Document = () => {
   return (
