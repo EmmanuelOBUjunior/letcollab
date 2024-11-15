75232d5c8d8ac7b63509fa0133f312a038396201
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-15 21:55:00 +0000

    Working on the header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image"
+import Link from "next/link"
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className="header">
+      <Link href='/'>
+      <Image
+      alt="Logo with name"
+      src='/assets/icons/logo.svg'
+      className="hidden md:block"
+      width={120}
+      height={32}
+      />
+      </Link>
     </div>
   )
 }
