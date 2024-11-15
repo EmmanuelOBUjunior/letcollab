54be3922ac1e4a7cda0b3125a0b6be0acb5b232d
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-15 21:19:47 +0000

    Setting up a design system

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      
+      <Button>Click Me</Button>
     </div>
   )
 }
