715809d89b3ccf0b3e9933efc44f86cd0023d2bb
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-15 23:47:26 +0000

    working on room actions

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -1,9 +1,14 @@
 import {nanoid} from 'nanoid'
 
-export const createDocument = ({userId, email}) =>{
+export const createDocument = ({userId, email}: CreateDocumentParams) =>{
     const roomId = nanoid()
 
     try{
+        const metadata = {
+            creatorId: userId,
+            email,
+            title: 'Untitled'
+        }
 
     }catch(error){
         console.log("Error happened while creating a room: ", error)
