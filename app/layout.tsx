06b88ef6709a1d56507541932c1cd6f555e8122a
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-15 22:18:09 +0000

    Added clerk themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
 import type { Metadata } from "next";
-import {Inter as FontSans} from 'next/font/google'
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
 const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans'
-})
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "LiveDocs",
-  description: "A live document collaboration application created with NextJs, Clerk for authentication, liveblock for live updates, and sentry for monitoring the application",
+  description:
+    "A live document collaboration application created with NextJs, Clerk for authentication, liveblock for live updates, and sentry for monitoring the application",
 };
 
 export default function RootLayout({
@@ -24,18 +25,19 @@ export default function RootLayout({
     <ClerkProvider
       appearance={{
         baseTheme: dark,
-        variables:{colorPrimary: "#3371FF",
-          fontSize: "16px"
-        }
+        variables: { colorPrimary: "#3371FF", fontSize: "16px" },
       }}
     >
-    <html lang="en">
-      <body
-        className= {cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
