424fe410aa9448337692e33319954bdb797ef797
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-15 22:17:50 +0000

    Added clerk themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import {Inter as FontSans} from 'next/font/google'
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -20,7 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables:{colorPrimary: "#3371FF",
+          fontSize: "16px"
+        }
+      }}
+    >
     <html lang="en">
       <body
         className= {cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}
