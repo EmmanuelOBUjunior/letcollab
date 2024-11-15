5a35cdf2980383bb1802f79dd7c57fb52f1332d1
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-15 23:01:58 +0000

    feat: working on the provider: liveblock room

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -8,13 +8,11 @@ import { ReactNode } from "react";
 const Provider = ({children}:{children:ReactNode}) => {
   return (
     <LiveblocksProvider publicApiKey={"pk_dev_PygIWMLt2YiQ5Nn_dqvIMlCw4ZPtcFCtSRbjQLcyYfZInhEQ9LxnMfRsWIZqi5v2"}>
-    <RoomProvider id="my-room">
       <ClientSideSuspense fallback={<div>Loading…</div>}>
         {children}
       </ClientSideSuspense>
-    </RoomProvider>
   </LiveblocksProvider>
   )
 }
 
-export default Provider
+export default Provider 
