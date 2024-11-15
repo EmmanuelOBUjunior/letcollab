d261a64adafd09841e2968ac32c62c990337d55d
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-15 23:11:45 +0000

    Working on the  collaborative room

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -8,7 +8,18 @@ const CollaborativeRoom = () => {
     <RoomProvider id="my-room">
       <ClientSideSuspense fallback={<Loader />}>
         <div className="collaborative-room">
-          
+        <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">Document title goes here</p>
+        </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </Header>
+      <Editor />
         </div>
       </ClientSideSuspense>
     </RoomProvider>
