6e74620c3d516dc22279493220c51ec98d872e4d
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-15 22:13:03 +0000

    Added clerk middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import {Inter as FontSans} from 'next/font/google'
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -19,6 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className= {cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}
@@ -26,5 +28,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
