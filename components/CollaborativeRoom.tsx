04b796ac5a5ee6acab998588b3b8568739967404
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-15 23:11:38 +0000

    Working on the  collaborative room

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -1,16 +1,18 @@
-'use client'
+"use client";
 
-import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense"
-import Loader from "./Loader"
+import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
+import Loader from "./Loader";
 
 const CollaborativeRoom = () => {
   return (
     <RoomProvider id="my-room">
-        <ClientSideSuspense fallback={<Loader/>}>
-          <div className="collaborative-room"></div>
-        </ClientSideSuspense>
-      </RoomProvider>
-  )
-}
+      <ClientSideSuspense fallback={<Loader />}>
+        <div className="collaborative-room">
+          
+        </div>
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
 
-export default CollaborativeRoom
+export default CollaborativeRoom;
