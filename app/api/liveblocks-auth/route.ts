aeacf732f7bcdb5c177a7612ca0025163f109f63
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-15 23:28:49 +0000

    setting up id token permissions for liveblock

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -1,10 +1,27 @@
+import { liveblocks } from "@/lib/liveblocks";
+import { currentUser } from "@clerk/nextjs/server";
 import { Liveblocks } from "@liveblocks/node";
+import { redirect } from "next/navigation";
 
 
 
 export async function POST(request: Request) {
   // Get the current user from your database
-  const user = __getUserFromDB__(request);
+  const clerkUser = await currentUser()
+  if(!clerkUser) redirect('/sign-in')
+
+    const {id. firstName, lastName, emailAddresses, imageUrl} = clerkUser
+    
+    const user= {
+        id,
+        info:{
+            id,
+            name: `${firstName} ${lastName}`,
+            email: emailAddresses[0].emailAddress,
+            avatar: imageUrl,
+            color: ''
+        }
+    }
 
   // Identify the user and return the result
   const { status, body } = await liveblocks.identifyUser(
