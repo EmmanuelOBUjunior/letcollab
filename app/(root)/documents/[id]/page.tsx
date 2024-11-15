6ca73f1e3a44aaac6d1fb1c2930e7f8db506fc50
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-15 22:20:47 +0000

    Working on the authentication

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { Editor } from "@/components/editor/Editor"
 import Header from "@/components/Header"
+import { SignedIn, UserButton } from "@clerk/clerk-react"
+import { SignedOut, SignInButton } from "@clerk/nextjs"
 
 const Document = () => {
   return (
@@ -8,6 +10,12 @@ const Document = () => {
         <div className="flex w-fit items-center justify-center gap-2">
           <p className="document-title">Document title goes here</p>
         </div>
+        <SignedOut>
+          <SignInButton/>
+        </SignedOut>
+        <SignedIn>
+          <UserButton/>
+        </SignedIn>
       </Header>
       <Editor/>
     </div>
