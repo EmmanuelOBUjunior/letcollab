cd386a770f8c4888653504ffcf72806fe1d84889
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-15 23:19:46 +0000

    setting up id token permissions for liveblock

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -1,8 +1,6 @@
 import { Liveblocks } from "@liveblocks/node";
 
-const liveblocks = new Liveblocks({
-  secret: "sk_dev_eaouNfBQPnVF8BRLe-mwtQ5lvO2cmjz-yAJixdGfGk3oxkMEqtMgHraA_cokDd1O",
-});
+
 
 export async function POST(request: Request) {
   // Get the current user from your database
