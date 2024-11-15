a8fd065e9774668eaba4f6258293f58b9a9ff179
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-15 21:02:19 +0000

    Setting up the font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import {Inter as FontSans} from 'next/font/google'
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className= {cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}
       >
         {children}
       </body>
