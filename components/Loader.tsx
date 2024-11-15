db46189ca20ab541404d2a7163598baffdbc9483
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-15 23:04:55 +0000

    Working on the loader component

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image"
 
 const Loader = () => {
   return (
     <div className="loader">
-      
+      <Image
+      alt="loader"
+      src='/assets/icons/loader.svg'
+      width={32}
+      height={32}
+      className="animate-spin"
+      />
     </div>
   )
 }
