45e085b6415ff76a7ce9937f039989516f8587ab
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 10:31:17 +0000

    Validating a user when a room is created

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -1,89 +1,36 @@
-'use client';
+"use client";
 
-import { ClientSideSuspense, RoomProvider } from '@liveblocks/react/suspense'
-import { Editor } from '@/components/editor/Editor'
-import Header from '@/components/Header'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
-import ActiveCollaborators from './ActiveCollaborators';
-import { useEffect, useRef, useState } from 'react';
-// import { Input } from './ui/input';
-import Image from 'next/image';
-// import { updateDocument } from '@/lib/actions/room.actions';
-import Loader from './Loader';
-// import ShareModal from './ShareModal';
+import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
+import Loader from "./Loader";
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
+import ActiveCollaborators from "./ActiveCollaborators";
 
 const CollaborativeRoom = ({ roomId, roomMetadata}: CollaborativeRoomProps) => {
-  const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
-  const [editing, setEditing] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const containerRef = useRef<HTMLDivElement>(null);
-  const inputRef = useRef<HTMLDivElement>(null);
-
-  // const updateTitleHandler = async (e: React.KeyboardEvent<HTMLInputElement>) => {
-  //   if(e.key === 'Enter') {
-  //     setLoading(true);
-
-  //     try {
-  //       if(documentTitle !== roomMetadata.title) {
-  //         const updatedDocument = await updateDocument(roomId, documentTitle);
-          
-  //         if(updatedDocument) {
-  //           setEditing(false);
-  //         }
-  //       }
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-
-  //     setLoading(false);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   const handleClickOutside = (e: MouseEvent) => {
-  //     if(containerRef.current && !containerRef.current.contains(e.target as Node)) {
-  //       setEditing(false);
-  //       updateDocument(roomId, documentTitle);
-  //     }
-  //   }
-
-  //   document.addEventListener('mousedown', handleClickOutside);
-
-  //   return () => {
-  //     document.removeEventListener('mousedown', handleClickOutside);
-  //   }
-  // }, [roomId, documentTitle])
-
-  // useEffect(() => {
-  //   if(editing && inputRef.current) {
-  //     inputRef.current.focus();
-  //   }
-  // }, [editing])
-  
-
   return (
     <RoomProvider id={roomId}>
       <ClientSideSuspense fallback={<Loader />}>
         <div className="collaborative-room">
           <Header>
-            
+            <div className="flex w-fit items-center justify-center gap-2">
+              <p className="document-title">Document title goes here</p>
+            </div>
             <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
               <ActiveCollaborators />
-
-              <SignedOut>
-                <SignInButton />
-              </SignedOut>
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
             </div>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
           </Header>
-        <Editor/>
+          <Editor />
         </div>
       </ClientSideSuspense>
     </RoomProvider>
-  )
-}
+  );
+};
 
-export default CollaborativeRoom
+export default CollaborativeRoom;
