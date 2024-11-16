9676ca533a26763a33f9729b1422e5f44479a793
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 21:58:16 +0000

    Working on invite and collaborator

--- components/Collaborator.tsx
+++ components/Collaborator.tsx
@@ -27,8 +27,9 @@ const Collaborator = ({
         <div>
           <p className="line-clamp-1 text-sm font-semibold leading-4 text-white">
             {collaborator.name}
-            <span className="text-sm font-light text-blue-100"> {loading && 'updating...'}</span>
+            <span className="text-10-regular pl-2 text-blue-100">{loading && 'updating...'}</span>
           </p>
+          <p className="text-sm font-light text-blue-100">{collaborator.email}</p>
         </div>
       </div>
     </li>
