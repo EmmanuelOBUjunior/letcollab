fd7cfe5269943beb2ce0953cac303d085b2640c0
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 07:46:29 +0000

    Added document button component

--- components/AddDocumentBtn.tsx
+++ components/AddDocumentBtn.tsx
@@ -3,11 +3,16 @@ import Image from "next/image";
 import React from "react";
 import { Button } from "./ui/button";
 import { createDocument } from "@/lib/actions/room.actions";
+import { useRouter } from "next/navigation";
 
 const AddDocumentBtn = ({ userId, email }: AddDocumentBtnProps) => {
+    const router = useRouter()
   const addDocumentHandler = async() => {
     try {
-        const room = createDocument({userId, email})
+        const room = await createDocument({userId, email})
+        if(room){
+            router.push(`/documents/${room.id}`)
+        }
     } catch (error) {
         console.log("Could not create a blank document", error)
     }
