dbe931cc0378dcc70d198f3076957ddfae6e4883
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 07:19:35 +0000

    feat:working on the homepage

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,10 @@
+import { cn } from "@/lib/utils"
 import Image from "next/image"
 import Link from "next/link"
 
-const Header = ({children}: HeaderProps) => {
+const Header = ({children, className}: HeaderProps) => {
   return (
-    <div className="header">
+    <div className={cn("header", className)}>
       <Link href='/'>
       <Image
       alt="Logo with name"
