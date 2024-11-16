5cf6d47a2649095b90f72aa14eaca833545c3b84
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 11:06:51 +0000

    working on editing the document name and share button

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -21,13 +21,13 @@ const CollaborativeRoom = ({ roomId, roomMetadata}: CollaborativeRoomProps) => {
     <RoomProvider id={roomId}>
       <ClientSideSuspense fallback={<Loader />}>
         <div className="collaborative-room">
-          <Header>
+          <Header className="">
             <div className="flex w-fit items-center justify-center gap-2">
               {editing && !loading ? (<Input/>):<>
               <p className="document-title">{documentTitle}</p>
               </>}
             </div>
-            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3 border border-white">
+            <div className="flex w-fit items-center justify-center gap-2">
               <ActiveCollaborators />
             <SignedOut>
               <SignInButton />
