21084b396fc84d19bf4a1d334096962c3fb1557e
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 20:50:06 +0000

    working share modal

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -1,5 +1,14 @@
 'use client'
+
+import { useSelf } from "@liveblocks/react/suspense"
+import { useState } from "react"
+
 const ShareModal = ({roomId, collaborators, creatorId, currentUserType}: ShareDocumentDialogProps) => {
+    const user = useSelf()
+    const [open, setOpen] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [email, setEmail] = useState('')
+    const [userType, setUserType] = useState<UserType>('viewer')
   return (
     <div>
       Share Modal
