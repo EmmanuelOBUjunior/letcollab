ab336ec7454c802e87ec45f3a3f746b95cefb714
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 13:15:30 +0000

    Working on a logic to get all documents

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,45 @@
-import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
-import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
-import Provider from "./Provider";
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+import './globals.css'
+import { Metadata } from "next"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
+import Provider from "./Provider"
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-});
+})
 
 export const metadata: Metadata = {
-  title: "LiveDocs",
-  description:
-    "A live document collaboration application created with NextJs, Clerk for authentication, liveblock for live updates, and sentry for monitoring the application",
-};
+  title: 'LiveDocs',
+  description: 'Your go-to collaborative editor',
+}
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider
       appearance={{
         baseTheme: dark,
-        variables: { colorPrimary: "#3371FF", fontSize: "16px" },
+        variables: { 
+          colorPrimary: "#3371FF" ,
+          fontSize: '16px'
+        },
       }}
     >
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body
           className={cn(
-            "min-h-screen bg-background font-sans antialiased",
+            "min-h-screen font-sans antialiased",
             fontSans.variable
           )}
         >
           <Provider>
-          {children}
+            {children}
           </Provider>
         </body>
       </html>
     </ClerkProvider>
-  );
+  )
 }
