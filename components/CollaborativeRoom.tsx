a9f4444d4ef25727ba05d172c4d11e39ebea685e
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 10:57:58 +0000

    working on editing the document name and share button

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -6,17 +6,25 @@ import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 import ActiveCollaborators from "./ActiveCollaborators";
+import { useRef, useState } from "react";
 
 const CollaborativeRoom = ({ roomId, roomMetadata}: CollaborativeRoomProps) => {
+  const [documentTitle, setDocumentTitle] = useState(roomMetadata.title)
+  const [editing, setEditing] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const containerRef = useRef<HTMLDivElement>(null)
+  const inputRef = useRef<HTMLDivElement>(null)
+
   return (
     <RoomProvider id={roomId}>
       <ClientSideSuspense fallback={<Loader />}>
         <div className="collaborative-room">
           <Header>
             <div className="flex w-fit items-center justify-center gap-2">
-              <p className="document-title">Document title goes here</p>
+              <p className="document-title">Share</p>
             </div>
-            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3 border border-white">
               <ActiveCollaborators />
             <SignedOut>
               <SignInButton />
