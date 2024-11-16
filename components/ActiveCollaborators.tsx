19f26c8e7068039455b4e73f2a4a2fc583db576e
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 08:36:28 +0000

    Working on the active collaborators list

--- components/ActiveCollaborators.tsx
+++ components/ActiveCollaborators.tsx
@@ -2,6 +2,8 @@ import { useOthers } from "@liveblocks/react/suspense"
 
 const ActiveCollaborators = () => {
     const others = useOthers()
+
+    const collaborators = others.map((other)=>other.info)
   return (
     <ul className="collaborators-list">
       
