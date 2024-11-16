7c7d6c645f11987b8f9a7e761224a606a4908354
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 07:38:32 +0000

    Added document button component

--- components/AddDocumentBtn.tsx
+++ components/AddDocumentBtn.tsx
@@ -1,13 +1,18 @@
-import Image from 'next/image'
-import React from 'react'
-import { Button } from './ui/button'
+import Image from "next/image";
+import React from "react";
+import { Button } from "./ui/button";
 
-const AddDocumentBtn = ({userId, email}: AddDocumentBtnProps) => {
+const AddDocumentBtn = ({ userId, email }: AddDocumentBtnProps) => {
   return (
     <Button>
-      <Image src='/assets/icons/add/svg' alt='Add document' width={24} height={24}/>
+      <Image
+        src="/assets/icons/add.svg"
+        alt="Add document"
+        width={24}
+        height={24}
+      />
     </Button>
-  )
-}
+  );
+};
 
-export default AddDocumentBtn
+export default AddDocumentBtn;
