6c64e515db139ffd3805bd636c4c48b171d8ecbb
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 07:21:38 +0000

    feat:working on the homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
+import Header from '@/components/Header'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
   return (
     <main className='home-container'>
-      <Button>Click Me</Button>
+      <Header className='sticky left-0 top-0'>
+        <Button>Create Blank Document</Button>
+      </Header>
     </main>
   )
 }
