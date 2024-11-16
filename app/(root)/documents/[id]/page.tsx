997549a4072c635e075c6ee16af5ba5a458cd151
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 09:33:31 +0000

    Validating a user when a room is created

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -3,14 +3,17 @@ import { getDocuments } from "@/lib/actions/room.actions";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-const Document = async({params:{id}}: SearchParamProps) => {
-  const clerkUser = await currentUser()
-  if(!clerkUser) redirect('/sign-in')
-  
-  const room = getDocuments({roomId:id, userId:clerkUser.emailAddresses[0].emailAddress})
+const Document = async ({ params: { id } }: SearchParamProps) => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect("/sign-in");
+
+  const room = getDocuments({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress,
+  });
   return (
     <main className="flex w-full flex-col items-center">
-      <CollaborativeRoom roomId= {id}/>
+      <CollaborativeRoom roomId={id} roomMetadata={roomMetadata} />
     </main>
   );
 };
