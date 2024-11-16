38bb6e8b007b1c8be2904d7b37b813e661cb87e9
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 12:36:56 +0000

    Working on a logic to get all documents

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -28,7 +28,7 @@ const Home = async () => {
         {roomDocuments.data.length > 0 ? (
           <div className="document-list-container">
              <div className="document-list-title">
-              <h3>All Documents</h3>
+              <h3 className="text-28-semibold">All Documents</h3>
               <AddDocumentBtn userId={clerkUser.id} email={clerkUser.emailAddresses[0].emailAddress}/>
               </div> 
           </div>
