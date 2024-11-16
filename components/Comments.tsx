3f1978ee11580e181a8b2c426245c336cb46fff7
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 15:55:29 +0000

    Working on the comments and threads

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,22 +1,23 @@
-import { cn } from '@/lib/utils';
-import { useIsThreadActive } from '@liveblocks/react-lexical';
-import { Composer, Thread } from '@liveblocks/react-ui';
-import { useThreads } from '@liveblocks/react/suspense';
+import { cn } from "@/lib/utils";
+import { useIsThreadActive } from "@liveblocks/react-lexical";
+import { Composer, Thread } from "@liveblocks/react-ui";
+import { useThreads } from "@liveblocks/react/suspense";
 
 const ThreadWrapper = ({ thread }: ThreadWrapperProps) => {
   const isActive = useIsThreadActive(thread.id);
 
   return (
-    <Thread 
+    <Thread
       thread={thread}
-      data-state={isActive ? 'active' : null}
-      className={cn('comment-thread border', 
-        isActive && '!border-blue-500 shadow-md',
-        thread.resolved && 'opacity-40'
+      data-state={isActive ? "active" : null}
+      className={cn(
+        "comment-thread border",
+        isActive && "!border-blue-500 shadow-md",
+        thread.resolved && "opacity-40"
       )}
     />
-  )
-}
+  );
+};
 
 const Comments = () => {
   const { threads } = useThreads();
@@ -29,7 +30,7 @@ const Comments = () => {
         <ThreadWrapper key={thread.id} thread={thread} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
