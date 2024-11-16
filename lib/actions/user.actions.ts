dc986b42eddf1dc1033e1e21d111dd7a94231e20
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 18:43:48 +0000

    Working on mentions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -39,12 +39,14 @@ export const getDocumentUsers = async ({
     const users = Object.keys(room.usersAccesses).filter(
       (email) => email !== currentUser
     );
-    if(text.length){
-        const lowerCaseText = text.toLowerCase()
-        const filteredUsers = users.filter((email:string)=>{email.toLowerCase().includes(lowerCaseText)})
-        return parseStringify(filteredUsers)
+    if (text.length) {
+      const lowerCaseText = text.toLowerCase();
+      const filteredUsers = users.filter((email: string) => {
+        email.toLowerCase().includes(lowerCaseText);
+      });
+      return parseStringify(filteredUsers);
     }
-return parseStringify(users)
+    return parseStringify(users);
   } catch (error) {
     console.log("Error fetching document users: ", error);
   }
