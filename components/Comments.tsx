fcc3e5713fbc11ee98da7b9a5e3882acaf35c742
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 15:43:49 +0000

    Working on the comments and threads

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,3 +1,4 @@
+import { Composer } from '@liveblocks/react-ui'
 import { useThreads } from '@liveblocks/react/suspense'
 import React from 'react'
 
@@ -5,7 +6,7 @@ const Comments = () => {
     const {threads} = useThreads()
   return (
     <div className='comments-container'>
-      
+      <Composer className='comment-composer'/>
     </div>
   )
 }
