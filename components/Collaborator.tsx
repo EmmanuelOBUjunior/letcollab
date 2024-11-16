bd569c7cdc44e2fca0c6b74d14ba72829fffa380
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 21:51:18 +0000

    Working on invite and collaborator

--- components/Collaborator.tsx
+++ components/Collaborator.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import { useState } from "react"
 
 const Collaborator = ({roomId, creatorId, email, collaborator, user}: CollaboratorProps) => {
@@ -8,11 +9,18 @@ const Collaborator = ({roomId, creatorId, email, collaborator, user}: Collaborat
     const removeCollaboratorHandler = async(email:string) => {}
 
   return (
-    <div>
-      collaborator
-    </div>
+    <li className="flex items-center justify-between gap-2 py-3">
+      <div className="flex gap-2">
+        <Image
+        src={collaborator.avatar}
+        alt={collaborator.name}
+        width= {36}
+        height={36}
+        className="size-9 rounded-full"
+        />
+      </div>
+    </li>
   )
 }
 
 export default Collaborator
-  
