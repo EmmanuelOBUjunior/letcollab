d56b16de0da00a63498f150c79f464fc2d6d0481
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 15:13:25 +0000

    Working on the collaborative functionality

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,6 @@
 import CollaborativeRoom from "@/components/CollaborativeRoom";
 import { getDocument } from "@/lib/actions/room.actions";
+import { getClerkUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -13,7 +14,13 @@ const Document = async ({ params}: SearchParamProps) => {
     userId: clerkUser.emailAddresses[0].emailAddress,
   });
 
-  //TODO: Access the permissions of the user to access the document
+  const userIds = Object.keys(room.usersAccesses)
+  const users = await getClerkUsers({userIds})
+
+  const userData = users.map((user:User)=>({
+      ...user,
+      userType: room.usersAccesses[user.email]?.includes('room:write') ? 'editor': 'viewer'
+  }))
 
   return (
     <main className="flex w-full flex-col items-center">
