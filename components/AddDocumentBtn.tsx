f68870e989235edab7b94ede8d30acf63ee6293a
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 07:42:02 +0000

    Added document button component

--- components/AddDocumentBtn.tsx
+++ components/AddDocumentBtn.tsx
@@ -1,14 +1,16 @@
-'use client'
+"use client";
 import Image from "next/image";
 import React from "react";
 import { Button } from "./ui/button";
 
 const AddDocumentBtn = ({ userId, email }: AddDocumentBtnProps) => {
-    const addDocumentHandler = ()=>{
-
-    }
+  const addDocumentHandler = () => {};
   return (
-    <Button type="submit" onClick={addDocumentHandler} className="gradient-blue flex gap-1 shadow-md">
+    <Button
+      type="submit"
+      onClick={addDocumentHandler}
+      className="gradient-blue flex gap-1 shadow-md"
+    >
       <Image
         src="/assets/icons/add.svg"
         alt="Add document"
