0e231cab8a6eef2393981692cf3f01d29e1381b2
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 07:50:11 +0000

    Added document button component

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -3,7 +3,7 @@ import CollaborativeRoom from "@/components/CollaborativeRoom";
 const Document = () => {
   return (
     <main className="flex w-full flex-col items-center">
-      {/* <CollaborativeRoom/> */}
+      <CollaborativeRoom/>
     </main>
   );
 };
