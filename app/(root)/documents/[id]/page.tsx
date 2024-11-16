af7bc04a00b13489f72912a1ea9bcde1c1b82e26
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 08:28:01 +0000

    Working on the collaborative room

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,8 +1,7 @@
 import CollaborativeRoom from "@/components/CollaborativeRoom";
-import { useParams } from "next/navigation";
+
 
 const Document = ({params:{id}}: SearchParamProps) => {
-  console.log(id)
   return (
     <main className="flex w-full flex-col items-center">
       <CollaborativeRoom roomId= {id}/>
