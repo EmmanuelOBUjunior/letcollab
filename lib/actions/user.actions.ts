e2f820737873e12c07b40f57ab3a7559d7b75ba2
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 08:52:37 +0000

    Working on user actions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -7,6 +7,9 @@ export const getClerkUsers = async({userIds}:{userIds: string[]})=>{
         const {data} = await clerkClient.users.getUserList({
             emailAddress: userIds,
         })
+
+        const users = data.
+
     } catch (error) {
         console.log("Could not get clerkusers", error)
     }
