47e89b9d78f61f5189ec949d0ecfe71bb0053223
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 07:34:01 +0000

    Added document button component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,34 +1,38 @@
-import Header from '@/components/Header'
-import { Button } from '@/components/ui/button'
-import { SignedIn, UserButton, UserProfile } from '@clerk/nextjs'
-import Image from 'next/image'
-import React from 'react'
+import Header from "@/components/Header";
+import { Button } from "@/components/ui/button";
+import { SignedIn, UserButton, UserProfile } from "@clerk/nextjs";
+import Image from "next/image";
+import React from "react";
 
 const Home = () => {
-  const documents = []
+  const documents = [];
   return (
-    <main className='home-container'>
-      <Header className='sticky left-0 top-0'>
-        <div className='flex items-center gap-2 lg:gap-2'>
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-2">
           Notifications
           <SignedIn>
-            <UserButton/>
+            <UserButton />
           </SignedIn>
         </div>
       </Header>
       <div>
-      {documents.length > 0 ? (<div></div>):(<div>
-        <Image
-        src='/assets/icons/doc.svg'
-        width={40}
-        height={40}
-        alt='Document'
-        className='mx-auto'
-        />
-      </div>)}
+        {documents.length > 0 ? (
+          <div></div>
+        ) : (
+          <div>
+            <Image
+              src="/assets/icons/doc.svg"
+              width={40}
+              height={40}
+              alt="Document"
+              className="mx-auto"
+            />
+          </div>
+        )}
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
