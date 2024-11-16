b2a531313ad8f89d5e40b8bb5c8ba3ec9af8bee7
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 15:42:20 +0000

    Working on the comments and threads

---
+++ components/Comments.tsx
@@ -0,0 +1,13 @@
+import { useThreads } from '@liveblocks/react/suspense'
+import React from 'react'
+
+const Comments = () => {
+    const {threads} = useThreads()
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Comments
