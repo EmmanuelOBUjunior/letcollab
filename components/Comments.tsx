16f57d469ed8a76df5c496e184f912d9f678b696
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 15:47:42 +0000

    Working on the comments and threads

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,12 +1,28 @@
-import { Composer } from '@liveblocks/react-ui'
+import { useIsThreadActive } from '@liveblocks/react-lexical'
+import { Composer, Thread } from '@liveblocks/react-ui'
 import { useThreads } from '@liveblocks/react/suspense'
 import React from 'react'
+
+
+const ThreadWrapper = ({thread}: ThreadWrapperProps) =>{
+    const isActive = useIsThreadActive(thread.id)
+
+    return(
+        <Thread
+        
+        />
+    )
+}
+
 
 const Comments = () => {
     const {threads} = useThreads()
   return (
     <div className='comments-container'>
       <Composer className='comment-composer'/>
+      {threads.map((thread)=>(
+        <ThreadWrapper key={thread.id} thread={thread}/>
+      ))}
     </div>
   )
 }
