01ee3d779d702647fdd21b27a8502b537ceadbec
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 08:28:29 +0000

    Working on the collaborative room

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,6 +1,5 @@
 import CollaborativeRoom from "@/components/CollaborativeRoom";
 
-
 const Document = ({params:{id}}: SearchParamProps) => {
   return (
     <main className="flex w-full flex-col items-center">
