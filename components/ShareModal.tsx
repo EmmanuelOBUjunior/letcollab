329e72f986079683aeee9f58bc81ce187bc40b30
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 20:54:42 +0000

    working share modal

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -1,43 +1,44 @@
-'use client'
+"use client";
 
-import { useSelf } from "@liveblocks/react/suspense"
-import { useState } from "react"
+import { useSelf } from "@liveblocks/react/suspense";
+import { useState } from "react";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-  
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
-const ShareModal = ({roomId, collaborators, creatorId, currentUserType}: ShareDocumentDialogProps) => {
-    const user = useSelf()
-    const [open, setOpen] = useState(false)
-    const [loading, setLoading] = useState(false)
-    const [email, setEmail] = useState('')
-    const [userType, setUserType] = useState<UserType>('viewer')
+const ShareModal = ({
+  roomId,
+  collaborators,
+  creatorId,
+  currentUserType,
+}: ShareDocumentDialogProps) => {
+  const user = useSelf();
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [userType, setUserType] = useState<UserType>("viewer");
 
-    const shareDocumentHandler = async()=>{
-
-    }
+  const shareDocumentHandler = async () => {};
 
   return (
     <Dialog>
-  <DialogTrigger>Open</DialogTrigger>
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
-</Dialog>
+      <DialogTrigger>Open</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-  )
-}
-
-export default ShareModal
+export default ShareModal;
