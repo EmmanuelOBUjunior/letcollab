a22787f4016ea87cdb7228a8c3a0a25ec2c90868
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 08:53:22 +0000

    Working on user actions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,14 +1,9 @@
 'use server'
 
-import { clerkClient } from "@clerk/nextjs/server"
 
 export const getClerkUsers = async({userIds}:{userIds: string[]})=>{
     try {
-        const {data} = await clerkClient.users.getUserList({
-            emailAddress: userIds,
-        })
-
-        const users = data.
+        
 
     } catch (error) {
         console.log("Could not get users", error)
