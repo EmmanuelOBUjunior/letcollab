8c5cda2a176c07478c72ff08ced96249e996a9cc
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 20:46:38 +0000

    working share modal

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ShareModal = () => {
+const ShareModal = ({roomId, collaborators, creatorId, currentUserType}: ShareDocumentDialogProps) => {
   return (
     <div>
       Share Modal
