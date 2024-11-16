cf59fac3d2dcddb4c84c25be7ace2ea999779069
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 21:47:05 +0000

    Working on invite and collaborator

--- components/Collaborator.tsx
+++ components/Collaborator.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 const Collaborator = ({roomId, creatorId, email, collaborator, user}: CollaboratorProps) => {
     const [loading, setLoading] = useState(false)
+    const [userType, setUserType] = useState(collaborator.userType || 'viewer')
   return (
     <div>
       collaborator
