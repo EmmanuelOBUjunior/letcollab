3c95e005d5d34832984ed636d7b665cc431d3669
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 21:18:42 +0000

    working on the usertype selector

--- components/UserTypeSelector.tsx
+++ components/UserTypeSelector.tsx
@@ -9,9 +9,17 @@ import {
   
 const UserTypeSelector = ({userType, setUserType, onClickHandler}: UserTypeSelectorParams) => {
   return (
-    <div>
-      
-    </div>
+    <Select>
+  <SelectTrigger className="w-[180px]">
+    <SelectValue placeholder="Theme" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectItem value="light">Light</SelectItem>
+    <SelectItem value="dark">Dark</SelectItem>
+    <SelectItem value="system">System</SelectItem>
+  </SelectContent>
+</Select>
+
   )
 }
 
