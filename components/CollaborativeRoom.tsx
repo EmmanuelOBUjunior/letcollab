15c482f2e8d00bfe384a33958759925e050ca082
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 08:12:36 +0000

    Working on the collaborative room

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -5,7 +5,7 @@ import Loader from "./Loader";
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
-import { RoomData } from "@liveblocks/node";
+
 
 const CollaborativeRoom = () => {
   // const {id} = roomData
