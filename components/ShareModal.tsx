a1208fcc65b22dcbb951d1e6d1a0b816398a706e
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 20:54:27 +0000

    working share modal

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -24,9 +24,19 @@ const ShareModal = ({roomId, collaborators, creatorId, currentUserType}: ShareDo
     }
 
   return (
-    <div>
-      Share Modal
-    </div>
+    <Dialog>
+  <DialogTrigger>Open</DialogTrigger>
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Are you absolutely sure?</DialogTitle>
+      <DialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </DialogDescription>
+    </DialogHeader>
+  </DialogContent>
+</Dialog>
+
   )
 }
 
