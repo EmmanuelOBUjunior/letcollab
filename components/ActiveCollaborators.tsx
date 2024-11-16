1f91fb7eccacfe91385494d0171eeae9ccdf49c5
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 08:38:50 +0000

    Working on the active collaborators list

--- components/ActiveCollaborators.tsx
+++ components/ActiveCollaborators.tsx
@@ -1,4 +1,5 @@
 import { useOthers } from "@liveblocks/react/suspense"
+import Image from "next/image"
 
 const ActiveCollaborators = () => {
     const others = useOthers()
@@ -6,7 +7,11 @@ const ActiveCollaborators = () => {
     const collaborators = others.map((other)=>other.info)
   return (
     <ul className="collaborators-list">
-      
+      {collaborators.map(({id, avatar, name, color})=>(
+        <li key={id}>
+            <Image src={avatar} height={100} width={100} alt={name} className="inline-block size-8 rounded-full ring-2 ring-dark-100"/>
+        </li>
+      ))}
     </ul>
   )
 }
