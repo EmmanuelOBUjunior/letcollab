a80f1587c6a13b3d952c40cffbb2867085ee1011
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 15:15:18 +0000

    Working on the collaborative functionality

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -4,8 +4,8 @@ import { getClerkUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-const Document = async ({ params}: SearchParamProps) => {
-  const { id } = await params 
+const Document = async ({ params }: SearchParamProps) => {
+  const { id } = await params;
   const clerkUser = await currentUser();
   if (!clerkUser) redirect("/sign-in");
 
@@ -14,15 +14,21 @@ const Document = async ({ params}: SearchParamProps) => {
     userId: clerkUser.emailAddresses[0].emailAddress,
   });
 
-  const userIds = Object.keys(room.usersAccesses)
-  const users = await getClerkUsers({userIds})
+  const userIds = Object.keys(room.usersAccesses);
+  const users = await getClerkUsers({ userIds });
 
-  const userData = users.map((user:User)=>({
-      ...user,
-      userType: room.usersAccesses[user.email]?.includes('room:write') ? 'editor': 'viewer'
-  }))
+  const userData = users.map((user: User) => ({
+    ...user,
+    userType: room.usersAccesses[user.email]?.includes("room:write")
+      ? "editor"
+      : "viewer",
+  }));
 
-  const currentUserType = room.usersAccesses[clerkUser.emailAddresses[0].emailAddress]?.includes('room:write') ? 'editor': 'viewer'
+  const currentUserType = room.usersAccesses[
+    clerkUser.emailAddresses[0].emailAddress
+  ]?.includes("room:write")
+    ? "editor"
+    : "viewer";
 
   return (
     <main className="flex w-full flex-col items-center">
