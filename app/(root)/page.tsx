db5bb4a18b64d46093e0a9529855d438739e447a
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-16 12:34:45 +0000

    Working on a logic to get all documents

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import AddDocumentBtn from "@/components/AddDocumentBtn";
 import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
+import { getDocuments } from "@/lib/actions/room.actions";
 import { SignedIn, UserButton, UserProfile } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
@@ -10,7 +11,9 @@ import React from "react";
 const Home = async () => {
   const clerkUser = await currentUser();
   if (!clerkUser) redirect("/sign-in");
-  const documents = [];
+
+  const roomDocuments  = await getDocuments(clerkUser.emailAddresses[0].emailAddress)
+
   return (
     <main className="home-container">
       <Header className="sticky left-0 top-0">
@@ -22,8 +25,12 @@ const Home = async () => {
         </div>
       </Header>
       <div>
-        {documents.length > 0 ? (
-          <div></div>
+        {roomDocuments.data.length > 0 ? (
+          <div>
+             <div>
+              <h3>All Documents</h3>
+              </div> 
+          </div>
         ) : (
           <div>
             <Image
