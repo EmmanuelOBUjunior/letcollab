136ceb8f8690bec396271ce927d5462886023b0f
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-17 21:43:14 +0000

    Working on the notifications

--- components/Notifications.tsx
+++ components/Notifications.tsx
@@ -1,16 +1,25 @@
+'use client'
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-  } from "@/components/ui/popover"
-  
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import Image from "next/image";
 
 const Notifications = () => {
   return (
-    <div>
-      Notification
-    </div>
-  )
-}
+    <Popover>
+      <PopoverTrigger>
+        <Image
+        src='/assets/icons/bell.svg'
+        width={24}
+        height={24}
+        alt="inbox"
+        />
+      </PopoverTrigger>
+      <PopoverContent>Place content for the popover here.</PopoverContent>
+    </Popover>
+  );
+};
 
-export default Notifications
+export default Notifications;
