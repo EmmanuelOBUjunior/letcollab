4bacf19d809c6b4d2ada9b4510185894fa80c6ab
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-17 22:21:47 +0000

    Working on the notifications

--- components/Notifications.tsx
+++ components/Notifications.tsx
@@ -74,7 +74,7 @@ const Notifications = () => {
                       />
                     ),
                     $documentAccess: (props) => (
-                        <InboxNotification.Custom {...props} title={props.inboxNotification.activities[0].data.title} aside>
+                        <InboxNotification.Custom {...props} title={props.inboxNotification.activities[0].data.title} aside={<InboxNotification.Icon className="bg-transparent"><Image src={props.inboxNotification.activities[0].data.avatar as string || ''} alt="avatar" width={36} height={36} className="rounded-full"/></InboxNotification.Icon>}>
                             {props.children}
                         </InboxNotification.Custom>
                     )
