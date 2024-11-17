1f7c71fadf526e3baba6cd99d8708debc7ea217d
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-17 22:04:15 +0000

    Working on the notifications

--- components/Notifications.tsx
+++ components/Notifications.tsx
@@ -4,8 +4,10 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { LiveblocksUIConfig } from "@liveblocks/react-ui";
 import { useInboxNotifications, useUnreadInboxNotificationsCount } from "@liveblocks/react/suspense";
 import Image from "next/image";
+import { ReactNode } from "react";
 
 const Notifications = () => {
 
@@ -26,7 +28,11 @@ const Notifications = () => {
         />
         {count > 0 && (<div className="absolute right-2 top-2 z-20 size-3 rounded-full bg-red-500"/>)}
       </PopoverTrigger>
-      <PopoverContent>Place content for the popover here.</PopoverContent>
+      <PopoverContent>
+        <LiveblocksUIConfig overrides={{INBOX_NOTIFICATION_TEXT_MENTION: (user:ReactNode)=>(<>{user} mentioened you</>)}}>
+
+        </LiveblocksUIConfig>
+      </PopoverContent>
     </Popover>
   );
 };
