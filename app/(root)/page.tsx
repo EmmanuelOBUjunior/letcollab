93905a22d1a3e683cb6a41ccbcc82a74b6ed2911
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-17 01:20:35 +0000

    working on the delete modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import AddDocumentBtn from "@/components/AddDocumentBtn";
+import DeleteModal from "@/components/DeleteModal";
 import Header from "@/components/Header";
 import { getDocuments } from "@/lib/actions/room.actions";
 import { dateConverter } from "@/lib/utils";
@@ -43,6 +44,7 @@ const Home = async () => {
                         <p className="text-sm font-light text-blue-100">Created about {dateConverter(createdAt)}</p>
                       </div>
                     </Link>
+                    <DeleteModal/>
                   </li>
                 ))}
               </ul>
